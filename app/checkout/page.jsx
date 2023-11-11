d85e4584873ab2a1e413bfd186dfa64d04c999a2
Aryan Vishwakarma <aryanongithub@gmail.com>
2023-11-11 09:11:45 +0530

    merged

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,56 +1,283 @@
-'use client'
-import BuyNow from '@/components/buttons/BuyNow'
-import { useSearchParams } from 'next/navigation'
-import React, { useEffect } from 'react'
-import { Input } from "@nextui-org/react";
-import axios from 'axios';
+"use client";
+import BuyNow from "@/components/buttons/BuyNow";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import React, { useState } from "react";
+import { Input, Checkbox, Button } from "@nextui-org/react";
 
 function page() {
-  const params = useSearchParams()
-  const amount = params.get('amount')
-  const order_id = params.get('order_id')
-  const quantity = params.get('quantity')
-  const price = params.get('price')
-  const totalPrice = params.get('totalPrice')
-  const user_id = params.get('user_id')
-  const email = params.get('email')
-  const product_sku = params.get('product_sku')
-  const getProductSku = async () => {
-    const sku = params.get('product_sku')
-    console.log(sku)
-    const res = await axios.post('/api/get-product-id', {
-      sku
-    })
-    console.log(res)
-  }
-  useEffect(() => {
-    getProductSku()
-  }, [])
+  const params = useSearchParams();
+  const amount = params.get("amount");
+  const order_id = params.get("order_id");
+  const product_sku = params.get("product_sku");
+  const quantity = params.get("quantity");
+  const price = params.get("price");
+  const totalPrice = params.get("totalPrice");
+  const user_id = params.get("user_id");
+  const email = params.get("email");
+
+  const [value, setValue] = useState(1);
+  const [shipping_address, setShipping_Address] = useState(null);
+  const [billing_address, setBilling_Address] = useState(null);
+  const [phone, setPhone] = useState(null);
+  const [check, setCheck] = useState(false);
+  const [gstin, setGstin] = useState(null);
+  const [address1, setaddress1] = useState({
+    strtName: "",
+    bldgName: "",
+    Landmark: "",
+    zipcode: "",
+  });
+  const [address2, setaddress2] = useState({
+    strtName: "",
+    bldgName: "",
+    Landmark: "",
+    zipcode: "",
+  });
+  const [isSelected, setIsSelected] = React.useState(false);
+
+  // For Shipping Address
+  const ShipAddress = (event) => {
+    let { name, value } = event.target;
+    setaddress1({
+      ...address1,
+      [name]: value,
+    });
+  };
+
+  // For Billing Address
+  const BillAdress = (event) => {
+    let { name, value } = event.target;
+    setaddress2({
+      ...address2,
+      [name]: value,
+    });
+  };
+
+  let shipngAddress = `Street Name: ${address1.strtName}, Building Name / Flat No: ${address1.bldgName}, Landmark: ${address1.Landmark}, Pincode: ${address1.zipcode}`;
+
+  let bilgAddress = `Street Name: ${address2.strtName}, Building Name / Flat No: ${address2.bldgName}, Landmark: ${address2.Landmark}, Pincode: ${address2.zipcode}, GSTIN No. : ${gstin}`;
+
+  const ProceedChk = () => {
+    setShipping_Address(shipngAddress);
+
+    setBilling_Address(check ? shipngAddress : bilgAddress);
+
+    console.log(check ? shipngAddress : bilgAddress);
+    console.log(data);
+  };
 
   return (
-    <main className='h-screen w-screen bg-slate-300 flex'>
-      <section className='w-2/3 h-fulll bg-red-200 p-4'>
-        <Input type="email" variant="bordered" label="Email" />
-        <Input type="email" variant="bordered" label="Email" />
-        <Input type="email" variant="bordered" label="Email" />
-        <Input type="email" variant="bordered" label="Email" />
-        <Input type="email" variant="bordered" label="Email" />
+    <main className="h-auto w-full">
+      <h2 className="w-full text-center p-2  font-semibold text-2xl  ">
+        Checkout
+      </h2>
+      <section className="flex gap-2 w-full h-full p-4 md:flex-row flex-col-reverse">
+        <section className="w-full md:w-3/4 h-full border-slate-400 rounded-md border  p-4 md:p-8 flex flex-col gap-3">
+          <div>
+            <h2 className="text-xl font-light ">Shipping Address:</h2>
 
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-2 mt-3">
+              <Input
+                type="text"
+                label="Street Name"
+                required
+                size="sm"
+                variant="bordered"
+                name="strtName"
+                labelPlacement="outside"
+                placeholder="Enter Street Name"
+                onChange={ShipAddress}
+              />
+              <Input
+                type="text"
+                label="Building Name"
+                required
+                name="bldgName"
+                size="sm"
+                variant="bordered"
+                placeholder="Enter Building Name"
+                labelPlacement="outside"
+                onChange={ShipAddress}
+              />
 
-        <BuyNow amount={amount}
-          order_id={order_id}
-          product_sku={product_sku}
-          quantity={quantity}
-          price={price}
-          totalPrice={totalPrice}
-          user_id={user_id}
-          email={email}
-        />
+              <Input
+                type="text"
+                label="Landmark"
+                placeholder="Enter Landmark Name"
+                required
+                size="sm"
+                variant="bordered"
+                name="Landmark"
+                labelPlacement="outside"
+                onChange={ShipAddress}
+              />
+
+              <Input
+                type="number"
+                label="Pincode"
+                placeholder="Enter Pincode"
+                required
+                size="sm"
+                variant="bordered"
+                name="zipcode"
+                labelPlacement="outside"
+                onChange={ShipAddress}
+              />
+            </div>
+          </div>
+          <hr />
+          <div>
+            <h2 className="text-xl font-light ">Billing Address:</h2>
+
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-2 mt-3">
+              <Input
+                type="text"
+                label="Street Name"
+                required
+                size="sm"
+                variant="bordered"
+                name="strtName"
+                labelPlacement="outside"
+                value={address2.strtName}
+                placeholder="Enter Street Name"
+                onChange={BillAdress}
+              />
+              <Input
+                type="text"
+                label="Building Name"
+                required
+                size="sm"
+                variant="bordered"
+                value={address2.bldgName}
+                name="bldgName"
+                placeholder="Enter Building Name"
+                labelPlacement="outside"
+                onChange={BillAdress}
+              />
+
+              <Input
+                type="text"
+                label="Landmark"
+                placeholder="Enter Landmark Name"
+                required
+                size="sm"
+                variant="bordered"
+                value={address2.Landmark}
+                name="Landmark"
+                labelPlacement="outside"
+                onChange={BillAdress}
+              />
+
+              <Input
+                type="number"
+                label="Pincode"
+                placeholder="Enter Pincode"
+                required
+                size="sm"
+                variant="bordered"
+                value={address2.zipcode}
+                name="zipcode"
+                labelPlacement="outside"
+                onChange={BillAdress}
+              />
+            </div>
+
+            <span className="mt-2 font-semibold text-xs opacity-70 flex items-center  gap-2">
+              <Checkbox
+                isSelected={isSelected}
+                onValueChange={setIsSelected}
+                onClick={(e) => {
+                  isSelected ? setCheck(true) : setCheck(false);
+
+                  isSelected
+                    ? setaddress2({
+                        ...address2,
+                      })
+                    : setaddress2({
+                        ...address1,
+                      });
+                }}
+              >
+                <span className="text-xs"> Same as shipping address</span>
+              </Checkbox>
+            </span>
+          </div>
+          <hr />
+
+          <div className="flex items-center gap-2">
+            <p className="font-light">Set Quantity</p>
+            <Button className="border flex w-36 justify-around p-1 rounded-md select-none mt-1 text-xl">
+              <p onClick={() => setValue((prev) => prev - 1)}>-</p>
+              <h2 className="font-semibold text-sm">
+                {" "}
+                {value < 1 ? setValue(1) : value}{" "}
+              </h2>
+              <p onClick={() => setValue((prev) => prev + 1)}>+</p>
+            </Button>
+          </div>
+          <div>
+            <hr />
+            <span className="flex flex-col  w-full md:w-64 ">
+              <Input
+                type="number"
+                size="sm"
+                labelPlacement="outside"
+                label="Phone No"
+                className="p-1 rounded"
+                placeholder="Enter Your Phone Number"
+                required
+                variant="bordered"
+                onChange={(e) => setPhone(e.target.value)}
+                min={9999999999}
+                max={9999999999}
+              />
+            </span>
+            <p className="font-semibold text-xs opacity-70">
+              This phone number should be active
+            </p>
+          </div>
+            <BuyNow
+              amount={amount}
+              order_id={order_id}
+              product_sku={product_sku}
+              quantity={quantity}
+              price={price}
+              totalPrice={totalPrice}
+              user_id={user_id}
+              email={email}
+            />
+        </section>
+        <section className="md:w-3/12 h-full p-2 w-full border-slate-400  rounded-md border  py-5">
+          <h2 className="font-light text-xl">Order Summary</h2>
+          <div>
+            <div className="w-full h-72 bg-white shadow rounded mt-2 bg-[url('/img/hero-pattern.svg')]">
+              <Image
+                src="/"
+                width={150}
+                height={150}
+                className="object-contain"
+                alt="image"
+              />
+            </div>
+            <span className="flex gap-2  mt-3">
+              <h3 className=" font-semibold  whitespace-nowrap">
+                Product name:
+              </h3>
+              <p className="opacity-75 truncate">Product Title</p>
+            </span>
+            <span className="flex gap-2">
+              <h3 className=" font-semibold ">Subtotal:</h3>{" "}
+              <p className="opacity-75">Rs. {2000 * value}</p>
+            </span>
+            <hr />
+            <span className="flex gap-2">
+              <h3 className=" font-semibold ">Total:</h3>{" "}
+              <p className="opacity-75">Rs. {3000 * value}</p>
+            </span>
+          </div>
+        </section>
       </section>
-      <section className='w-1/3 h-full bg-blue-200'>
-
-      </section>
-
     </main>
   );
 }
--- components/SkuPage/CtaButton.jsx
+++ components/SkuPage/CtaButton.jsx
@@ -1,17 +1,61 @@
 'use client'
 import React from 'react'
-import {Button} from "@nextui-org/react";
-import Checkout from '../buttons/Checkout';
+import { Button } from "@nextui-org/react";
+import axios from "axios"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import { useToast } from "../ui/use-toast"
 
-const CtaButton = ({amount, price, totalPrice, sku}) => {
-    const style=""
+const CtaButton = ({ amount, price, totalPrice, sku }) => {
+  const router = useRouter()
+  const [userId, setUserId] = useState('')
+  const { data: session } = useSession()
+  const { toast } = useToast()
+  const userData = async () => {
+    const email = session?.user?.email;
+    const order = false
+    if (!email) {
+      toast({
+        title: 'Please log in to continue - Redirecting to login page'
+      })
+      return;
+    }
+    const res = await axios.post('/api/get-user-by-id', {
+      email,
+      order
+    })
+    setUserId(res.data.res._id)
+    return res;
+  }
+  const checkout = async () => {
+    try {
+      const response = await axios.post('/api/checkout', {
+        amount
+      })
+      const { data } = response;
+      router.push(`/checkout?amount=${data.order.amount}&order_id=${data.order.id}&product_sku=${sku}&quantity=${5}&price=${price}&totalPrice=${totalPrice}&user_id=${userId}&email=${session?.user?.email}`)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+  useEffect(() => {
+    userData()
+  }, [])
   return (
     <div className='grid grid-cols-2 gap-3 px-4'>
-     
-  <Button  color="primary" variant="bordered" className=" h-12 rounded-md border border-Primary">
+
+      <Button color="primary" variant="bordered" className=" h-12 rounded-md border border-Primary">
         Add to cart
       </Button>
-     <Checkout amount={amount} price={price} totalPrice={totalPrice} sku={sku} />
+      <Button onClick={() => {
+        const out = userData()
+        if (!out) {
+          checkout();
+        }
+      }} className="bg-Primary h-12 rounded-md border border-Primary" >
+        Buy now
+      </Button>
     </div>
   )
 }
