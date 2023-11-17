b12a6b040562e99a6fef1f2e4474c776b335beef
Developer-Devyansh <devyanshyadav41@gmail.com>
2023-11-17 12:03:18 +0530

    nodemail changes done

--- utils/services/mail.js
+++ utils/services/mail.js
@@ -1,27 +1,32 @@
 import nodemailer from 'nodemailer'
 
 export async function sendMail(subject, toEmail, html) {
-    var transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.NODEMAILER_EMAIL,
-        pass: process.env.NODEMAILER_PW,
-      },
-    });
-  
-    var mailOptions = {
-      from: process.env.NODEMAILER_EMAIL,
-      to: toEmail,
-      subject: subject,
-      html: html
-    };
-  
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.NODEMAILER_EMAIL,
+      pass: process.env.NODEMAILER_PW,
+    },
+  });
+
+  var mailOptions = {
+    from: process.env.NODEMAILER_EMAIL,
+    to: toEmail,
+    subject: subject,
+    html: html
+  };
+
+  await new Promise((res, rej) => {
     transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
         throw new Error(error);
+        rej(error)
       } else {
+        res(info)
         console.log("Email Sent");
         return true;
       }
     });
-  }
+  })
+
+}
