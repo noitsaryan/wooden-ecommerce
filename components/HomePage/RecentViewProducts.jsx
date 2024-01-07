33a6e56995f779ce0dd1dba1e69191610f0600db
Developer-Devyansh <devyanshyadav41@gmail.com>
2024-01-07 16:24:39 +0530

    added carousel

--- components/HomePage/RecentViewProducts.jsx
+++ components/HomePage/RecentViewProducts.jsx
@@ -37,32 +37,31 @@ const RecentViewProducts = () => {
           RECENTLY VIEWED
         </p>
       )}
-      <section className="w-full  p-2 gap-6">
+      <section className="w-full p-4 gap-6">
         <Carousel
  additionalTransfrom={0}
   arrows
   autoPlaySpeed={3000}
   centerMode={false}
-  className=""
   containerClass="container-with-dots"
   dotListClass=""
   draggable
   focusOnSelect={false}
   infinite
-  itemClass=""
   keyBoardControl
   minimumTouchDrag={80}
   pauseOnHover
   renderArrowsWhenDisabled={false}
   renderButtonGroupOutside={false}
   renderDotsOutside={false}
+
   responsive={{
     desktop: {
       breakpoint: {
         max: 3000,
         min: 1024
       },
-      items: 3,
+      items: 4,
       partialVisibilityGutter: 40
     },
     mobile: {
@@ -101,7 +100,7 @@ const RecentViewProducts = () => {
               link={e.images}
             />
           ))}
-</Carousel>;
+</Carousel>
        
       </section>
     </>
