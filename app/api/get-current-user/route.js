8950717e22d788382a20cc55d6d68027cf387908
Aryan Vishwakarma <aryanongithub@gmail.com>
2024-01-08 14:48:57 +0530

    fix(MyOrders.jsx): (4) Trying again after converting to async await method

--- app/api/get-current-user/route.js
+++ app/api/get-current-user/route.js
@@ -4,39 +4,47 @@ import { decode } from "jsonwebtoken";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
+async function getUserData() {
+  const userSessionCookie = cookies().get("ashofy-user-session");
+
+  if (!userSessionCookie) {
+    return {
+      success: false,
+      message: "User not logged in",
+    };
+  }
+
+  const decodedToken = await decode(userSessionCookie.value);
+
+  if (!decodedToken) {
+    return {
+      success: false,
+      message: "Invalid user session",
+    };
+  }
+
+  await connectDB();
+
+  const user = await User.findOne({ _id: decodedToken._id }).exec();
+
+  if (!user) {
+    return {
+      success: false,
+      message: "User does not exist",
+    };
+  }
+
+  return {
+    success: true,
+    message: "User fetched",
+    data: user,
+  };
+}
+
 export async function GET() {
   try {
-    const userSessionCookie = cookies().get("ashofy-user-session");
-    
-    if (!userSessionCookie)
-      return NextResponse.json({
-        success: false,
-        message: "User not logged in",
-      });
-
-    const decodedToken = await decode(userSessionCookie.value);
-
-    if (!decodedToken)
-      return NextResponse.json({
-        success: false,
-        message: "Invalid user session",
-      });
-
-    await connectDB();
-
-    const user = await User.findOne({ _id: decodedToken._id }).exec();
-
-    if (!user)
-      return NextResponse.json({
-        success: false,
-        message: "User does not exist",
-      });
-
-    return NextResponse.json({
-      success: true,
-      message: "User fetched",
-      data: user,
-    });
+    const userData = await getUserData();
+    return NextResponse.json(userData);
   } catch (error) {
     return NextResponse.json({
       success: false,
